38a2400556704cd755128c86ab4e0dce3d8a6c5d
subodh <subodhmaharjan33@gmail.com>
2021-02-17 18:38:46 +1100

    gon

--- server.js
+++ server.js
@@ -33,32 +33,16 @@ app.use(cors())
 let test_api = require('./server/routes/test_api');
 app.use(express.static(path.join(__dirname,'angular')));
 //Sending a GET to localhost:8080/dummy should return this
-app.get('/dummy', (req, res) => res.send('Response from Route of the Express Server!!'))
+const port= process.env.PORT || 3000;
 
-app.listen(4600);
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//
-app.use('/test_api', test_api);
-
-console.log("Server running on 8080")
-// app.get('*',(req,res)=>
-// {
-//   res.sendFile(path.join(__dirname,'dist/index.html'))
-// })
+app.get('*',(req,res)=>
+{
+  res.sendFile(path.join(__dirname,'angular/index.html'))
+})
 
 
+
+app.listen(port);
 
 
 module.exports = app;
